6de0686af07f19bb6de4436c2e8707a7ffe4c4b0
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-10 05:22:40 +0530

    feat: add front-end to app
    add Account and Bonus component
    
    manage state using useState

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Account from "./components/Account";
+import Bonus from "./components/Bonus";
+
+function App() {
+  return (
+    <div className="App">
+      <h4>App</h4>
+      <h3>Current amount : </h3>
+      <h3>Bonus : </h3>
+      <hr />
+      <Account></Account>
+      <hr />
+      <Bonus></Bonus>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Account.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function Account() {
+  const [account, setAccount] = useState({ amount: 0 });
+  const [value, setValue] = useState(0);
+
+  function increment() {
+    setAccount({ amount: account.amount + 1 });
+  }
+  function decrement() {
+    setAccount({ amount: account.amount - 1 });
+  }
+  function incrementByAmount(value) {
+    setAccount({ amount: account.amount + value });
+  }
+
+  return (
+    <div>
+      <h4>Account component</h4>
+      <h3>Amount : {account.amount}</h3>
+      <button onClick={increment}>Increment +</button>
+      <button onClick={decrement}>Decrement -</button>
+      <input type="text" onChange={(e) => setValue(+e.target.value)} />
+      <button onClick={() => incrementByAmount(value)}>
+        Increment By {value}
+      </button>
+    </div>
+  );
+}
